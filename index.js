750d7834432cf56235f8144f6034e2d7fb288bf7
sgokulraj <116162072+sgokulraj@users.noreply.github.com>
2023-08-08 08:01:17 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 const express = require("express")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
 const cors = require("cors")
 
-const dotenv = require("dotenv")
-const mongoose = require("mongoose")
 const userRoutes = require("./routes/userRoutes.js")
 const productRoutes = require("./routes/productRoutes.js")
 const imageRoutes = require("./routes/imageRoutes.js")
 const orderRoutes = require("./routes/orderRoutes.js")
-
+dotenv.config();
 const app = express()
 
-dotenv.config();
 const PORT = process.env.PORT;
 const CONNECTION_URL = process.env.CONNECTION_URL;
 const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY
 const stripe = require("stripe")(STRIPE_SECRET_KEY)
 
-app.use(cors());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+app.use(cors());
 app.use("/users", userRoutes)
 app.use("/products", productRoutes)
 app.use("/images", imageRoutes)
